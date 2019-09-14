fee65f06955433aaf1520fcc4126fcf9a9ed07c9
chapaykos <chapaykos@gmail.com>
2019-09-14 21:59:54 +0200

    fix database name to charity-donation

--- stuff_giver/settings.py
+++ stuff_giver/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'stuff_giver.wsgi.application'
 DATABASES = {
     'default': {
         'HOST': '127.0.0.1',
-        'NAME': 'give_it_away',
+        'NAME': 'charity_donation',
         'ENGINE': 'django.db.backends.postgresql',
         'USER': 'postgres',
         'PASSWORD': 'coderslab',
