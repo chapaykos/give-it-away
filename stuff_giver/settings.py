7cb21fc89be1cbb93b31c51cf19389dc8dfd6586
chapaykos <chapaykos@gmail.com>
2019-09-14 21:56:53 +0200

    settings.py configuration

--- stuff_giver/settings.py
+++ stuff_giver/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'stuff_giver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': '127.0.0.1',
+        'NAME': 'give_it_away',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
     }
 }
 
