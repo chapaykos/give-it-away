ed4053331d8254fc6060b5a42ffd3ff6fa528967
chapaykos <chapaykos@gmail.com>
2019-09-17 18:31:35 +0200

    html templates and base.html

--- charity_donation/views.py
+++ charity_donation/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class LandingPage(View):
+    return render(request, 'charity_donation/index.html')
+class AddDonation(View):
+
+class Login(View):
+
+class Register(View):
